3a03eba8bc43da38b807762255325be4c04b30e0
Dev-sky <201912032@daiict.ac.in>
2020-12-06 00:18:25 +0530

    Signup added

---
+++ app/src/main/java/com/daiict/enterprizecomputing/reviewdekho/SignUp/SignUp.java
@@ -0,0 +1,16 @@
+package com.daiict.enterprizecomputing.reviewdekho.SignUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.daiict.enterprizecomputing.reviewdekho.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
